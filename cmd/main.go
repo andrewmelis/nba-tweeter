72668e5259f68f9d4ec2e2fcce418f4646690cc9
Andrew Melis <andrewmelis@gmail.com>
2017-04-29 13:17:46 -0500

    Add PrintGameWatcher for testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/andrewmelis/nba-tweeter/game"
+	"github.com/andrewmelis/nba-tweeter/nba"
 	"github.com/andrewmelis/nba-tweeter/schedule"
 )
 
 func main() {
-	// s := NewNBASchedule()
+	s := nba.NewDefaultNBASchedule()
+	w := PrintGameWatcher{}
+	watch(w, s)
+
+	// fmt.Printf("%s\n", s.ScheduledGames())
 	// w := NewNBAWatcher()
 }
 
@@ -17,4 +24,16 @@ type GameWatcher interface {
 
 func watch(w GameWatcher, s schedule.Schedule) {
 	w.Follow(s)
+}
+
+type PrintGameWatcher struct {
+	s schedule.Schedule
+}
+
+func (w PrintGameWatcher) Follow(s schedule.Schedule) {
+	fmt.Printf("%#v\n", s.ScheduledGames())
+}
+
+func (w PrintGameWatcher) IsWatching(g game.Game) bool {
+	return true
 }
