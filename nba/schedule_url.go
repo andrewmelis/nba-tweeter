415b0739deefc7ac5b7529d335604c31b4e15a29
Andrew Melis <andrewmelis@gmail.com>
2017-04-29 17:24:01 -0500

    MVP version of schedule url retrieval
    Better version would be retrieving the value of "currentScoreboard" from
    the "nba today url"

--- nba/schedule_url.go
+++ nba/schedule_url.go
@@ -1,5 +1,10 @@
 package nba
 
+import (
+	"fmt"
+	"time"
+)
+
 type ScheduleURL interface {
 	URL() string
 }
@@ -11,5 +16,10 @@ func NewDefaultNBAScheduleURL() ScheduleURL {
 }
 
 func (u NBAScheduleURL) URL() string {
-	return "https://data.nba.net/data/10s/prod/v1/20170430/scoreboard.json"
+	// TODO retrieve from "today url":
+	// https://data.nba.net/10s/prod/v1/today.json -> currentScoreboard
+	now := time.Now()
+	date := now.Format("20060102")
+	fmt.Println(date)
+	return fmt.Sprintf("https://data.nba.net/data/10s/prod/v1/%s/scoreboard.json", date)
 }
