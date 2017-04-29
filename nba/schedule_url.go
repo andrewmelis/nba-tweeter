5034288d88f2b50d7cafc93ab2549bdebbe30bd2
Andrew Melis <andrewmelis@gmail.com>
2017-04-29 13:17:32 -0500

    Add WIP ScheduleURL implementation

--- nba/schedule_url.go
+++ nba/schedule_url.go
@@ -3,3 +3,13 @@ package nba
 type ScheduleURL interface {
 	URL() string
 }
+
+type NBAScheduleURL struct{}
+
+func NewDefaultNBAScheduleURL() ScheduleURL {
+	return NBAScheduleURL{}
+}
+
+func (u NBAScheduleURL) URL() string {
+	return "https://data.nba.net/data/10s/prod/v1/20170430/scoreboard.json"
+}
