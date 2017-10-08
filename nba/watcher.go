35d0fd798a1d25f8ad9166908cfc5a81272f1079
Andrew Melis <andrewmelis@gmail.com>
2017-10-07 23:04:45 -0500

    Avoid duplicate plays

--- nba/watcher.go
+++ nba/watcher.go
@@ -5,20 +5,46 @@ import (
 
 	"github.com/andrewmelis/nba-tweeter/clock"
 	"github.com/andrewmelis/nba-tweeter/game"
+	"github.com/andrewmelis/nba-tweeter/play"
 	"github.com/andrewmelis/nba-tweeter/processor"
 )
 
 type NBAWatcher struct {
-	c clock.Clock
-	p processor.Processor
+	c         clock.Clock
+	p         processor.Processor
+	seenPlays []play.Play
 }
 
 func NewNBAWatcher(c clock.Clock, p processor.Processor) NBAWatcher {
-	return NBAWatcher{c, p}
+	return NBAWatcher{
+		c:         c,
+		p:         p,
+		seenPlays: make([]play.Play, 0),
+	}
 }
 
 func (w *NBAWatcher) Follow(g game.Game) {
-	for _, play := range g.Plays() {
-		w.p.Process(g.GameCode(), play)
+	inputs := g.Plays()
+	inputs = unique(inputs)
+
+	for i, play := range inputs {
+		if i >= len(w.seenPlays) {
+			w.seenPlays = append(w.seenPlays, play)
+			w.p.Process(g.GameCode(), play)
+		}
 	}
+}
+
+func unique(in []play.Play) []play.Play {
+	found := make(map[string]bool)
+	out := make([]play.Play, 0)
+
+	for _, candidate := range in {
+		if found[candidate.String()] {
+			continue
+		}
+		found[candidate.String()] = true
+		out = append(out, candidate)
+	}
+	return out
 }
