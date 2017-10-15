299ba9093facab001244ef40573beebbf64ce067
Andrew Melis <andrewmelis@gmail.com>
2017-10-15 09:58:53 -0500

    Remove test; will use one game per watcher

--- nba/watcher_test.go
+++ nba/watcher_test.go
@@ -41,51 +41,6 @@ func TestFollowProcessesEachPlayOfGameOnce(t *testing.T) {
 	}
 }
 
-// FIXME -- one game per watcher
-func TestFollowProcessesMultipleGames(t *testing.T) {
-	playsG1 := []play.Play{
-		fakePlay{"play 1"},
-		fakePlay{"play 2"},
-	}
-
-	playsG2 := make([]play.Play, len(playsG1))
-	copy(playsG2, playsG1)
-
-	game1 := newFakeGame("GSWCLE", playsG1)
-	game2 := newFakeGame("OKCSAS", playsG2)
-
-	now := clock.MakeTime("20170609 7:30pm", "US/Eastern")
-	c := clock.NewFakeClock(now)
-
-	p := processor.NewDebugProcessor()
-	w := NewNBAWatcher(c, p, func(string) {})
-
-	w.Follow(game1)
-	w.Follow(game2)
-
-	c.Advance()
-	c.Advance() // HACK -- clock is broken
-
-	t.Errorf("CLOCK GOROUTINE ONLY WORKS WITH ONE SUBSCRIBER")
-	games := []*fakeGame{game1, game2}
-	plays := [][]play.Play{playsG1, playsG2}
-	for i, _ := range games {
-		expected := plays[i]
-		game := games[i].GameCode()
-		actual := p.Plays(games[i].GameCode())
-
-		if len(expected) != len(actual) {
-			t.Errorf("Game: %s, Wanted: %s, Got: %s", game, expected, actual)
-		}
-
-		for i, _ := range actual {
-			if expected[i] != actual[i] {
-				t.Errorf("Game: %s, Wanted: %s, Got: %s", game, expected, actual)
-			}
-		}
-	}
-}
-
 func TestFollowProcessesPeriodically(t *testing.T) {
 	plays := []play.Play{
 		fakePlay{"play 1"},
