e60256801246a50305df783d45e1b9eece07c819
Andrew Melis <andrewmelis@gmail.com>
2017-10-21 22:19:43 -0500

    Add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"fmt"
-
-	_ "github.com/andrewmelis/nba-tweeter/nba"
-	_ "github.com/andrewmelis/nba-tweeter/watcher"
+	"github.com/andrewmelis/nba-tweeter/nba"
+	"github.com/andrewmelis/nba-tweeter/processor"
 )
 
 func main() {
-	// s := nba.NewDefaultNBASchedule()
+	ch := make(chan struct{})
 
-	// w := watcher.NewMultiWatcher(TweetWatcher, LogWatcher, DBWatcher) ???
-	// w := watcher.NewDebugWatcher()
+	s := nba.NewNBASchedule(nba.NewNBAScheduleURL("ljsdaf"))
+	f := nba.NewNBAFollower(processor.ProcessorForGame)
 
-	// loops over games
-	// when new game
-	// initialize game
-	// game updates self with plays
-	// watcher pipes all plays to a sink (e.g. twitter sink, log sink)
+	f.Follow(s)
 
-	// w.Follow(s)
-	fmt.Printf("%s\n", "FIXME")
+	<-ch
 }
